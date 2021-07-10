668c76524c12b870c33edeabf84946146d09e009
karolinaq20 <karolinaq20@hotmail.com>
2021-07-09 21:35:25 -0300

    Atualização blogPessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,7 +1,9 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.generation.blogPessoal.Repository.UsuarioRepository;
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.services.UsuarioServices;
@@ -9,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +26,27 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class UsuarioController {
 	
+	@Autowired
+	private UsuarioRepository repository;
 	
 	@Autowired
 	private UsuarioServices usuarioService;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user)
 				.map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping("/cadastrar")
@@ -38,5 +55,8 @@ public class UsuarioController {
 				.body(usuarioService.CadastrarUsuario(usuario));
 	}
 
-
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
+	}
 }
